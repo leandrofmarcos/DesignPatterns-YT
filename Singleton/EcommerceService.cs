81c0ddace3580cdb77f0a2aeca4300a9a55efd55
Leandro Marcos <leandrofmarcos@gmail.com>
2023-08-14 17:57:52 -0300

    Add singleton project

---
+++ Singleton/EcommerceService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class EcommerceService
+    {
+        private Logger logger;
+
+        public EcommerceService()
+        {
+            logger = Logger.GetInstance();
+
+            var logger2 = Logger.GetInstance();
+        }
+
+        public void ProccessOrder(string orderId)
+        {
+            logger.Log($"Order {orderId} prcessed successfully.");
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,10 @@
+﻿using Singleton;
+
+public class Program
+{
+    static void Main()
+    {
+        EcommerceService ecommerce = new EcommerceService();
+        ecommerce.ProccessOrder("12345");
+    }
+}
